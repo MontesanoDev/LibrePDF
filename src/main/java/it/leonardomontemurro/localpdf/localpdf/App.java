3909f2425b7dc551a7ed9961d418cbcae3b07603
MontesanoDev <221404985+MontesanoDev@users.noreply.github.com>
2025-12-11 23:32:47 +0100

    Added AtlantaFx' in pom.xml

---
+++ src/main/java/it/leonardomontemurro/localpdf/localpdf/App.java
@@ -0,0 +1,28 @@
+package it.leonardomontemurro.localpdf.localpdf;
+
+import atlantafx.base.theme.Dracula;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class HelloApplication extends Application {
+    @Override
+    public void start(Stage primaryStage){
+        Application.setUserAgentStylesheet(new Dracula().getUserAgentStylesheet());
+
+        StackPane root = new StackPane();
+
+        Scene scene = new Scene(root, 300, 250);
+        primaryStage.setTitle("LocalPDF");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
