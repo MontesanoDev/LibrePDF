b0290445d8ce14ccfe8b55526872297a7362ba39
MontesanoDev <221404985+MontesanoDev@users.noreply.github.com>
2025-12-18 21:36:30 +0100

    Setup root scene

--- src/main/java/it/leonardomontemurro/localpdf/View.java
+++ src/main/java/it/leonardomontemurro/localpdf/View.java
@@ -5,38 +5,43 @@ import javafx.application.Application;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.StackPane;
 
 public class View {
     private final static int HEIGHT_SIZE = 400;
     private final static int WIDTH_SIZE = 600;
-
+    private final static int GRID_GAP = 15;
     private Scene scene;
 
     public void setGlobalTheme(){
         Application.setUserAgentStylesheet(new Dracula().getUserAgentStylesheet());
     }
     public void initializeScene(){
-        StackPane root = new StackPane();
+        AnchorPane root = new AnchorPane();
         BorderPane borderPane = new BorderPane();
         GridPane gridPane = new GridPane();
+
+        gridPane.setHgap(GRID_GAP);
+        gridPane.setVgap(GRID_GAP);
+        gridPane.setAlignment(Pos.CENTER);
+        AnchorPane.setTopAnchor(borderPane,0.0);
+        AnchorPane.setBottomAnchor(borderPane,0.0);
+        AnchorPane.setLeftAnchor(borderPane,0.0);
+        AnchorPane.setRightAnchor(borderPane,0.0);
         borderPane.setCenter(gridPane);
 
-        gridPane.setHgap(10);
-        gridPane.setVgap(10);
-        gridPane.setAlignment(Pos.CENTER);
-        gridPane.setGridLinesVisible(true);//debugging reason
+        Button testButton = new Button();
+        testButton.setText("I'm Test Button!");
 
-        Button button = new Button();
-        button.setText("Prova");
-        gridPane.add(button,0,0);
+        gridPane.getChildren().add(testButton);
+
         root.getChildren().add(borderPane);
         setScene(root);
     }
 
-    private void setScene(StackPane root){
+    private void setScene(AnchorPane root){
         this.scene = new Scene(root, WIDTH_SIZE, HEIGHT_SIZE);
     }
 
