fc4cf02fed1b295b8fff7c5ced891182e26f9207
Leonardo Montemurro <221404985+MontesanoDev@users.noreply.github.com>
2026-03-18 13:08:00 +0100

    feat: updated module info with core package

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,9 +4,14 @@ module it.leonardomontemurro.librepdf.localpdf {
 
     requires org.controlsfx.controls;
     requires atlantafx.base;
+    requires org.apache.pdfbox;
+    requires org.apache.pdfbox.io;
+    requires java.xml;
 
     opens it.leonardomontemurro.librepdf to javafx.fxml;
     exports it.leonardomontemurro.librepdf;
     exports it.leonardomontemurro.librepdf.ui;
     opens it.leonardomontemurro.librepdf.ui to javafx.fxml;
+    exports it.leonardomontemurro.librepdf.core;
+    opens it.leonardomontemurro.librepdf.core to javafx.fxml;
 }
