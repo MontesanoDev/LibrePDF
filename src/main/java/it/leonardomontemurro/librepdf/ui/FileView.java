25f378c4ccc3374e2512939e5166da174779e40d
Leonardo Montemurro <221404985+MontesanoDev@users.noreply.github.com>
2026-03-18 14:57:12 +0100

    refractor: removed unused import

--- src/main/java/it/leonardomontemurro/librepdf/ui/FileView.java
+++ src/main/java/it/leonardomontemurro/librepdf/ui/FileView.java
@@ -25,11 +25,7 @@ import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
-import javafx.scene.control.TextField;
 import javafx.scene.layout.*;
-import javafx.scene.text.TextAlignment;
-
-import java.io.File;
 
 public class FileView {
     private final BorderPane borderPane = new BorderPane();
