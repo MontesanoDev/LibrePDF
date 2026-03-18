3271c2cfe7b75e7c11a6d62140b96f620251406d
Leonardo Montemurro <221404985+MontesanoDev@users.noreply.github.com>
2026-03-18 21:50:56 +0100

    removed blank line

--- src/main/java/it/leonardomontemurro/librepdf/core/Unprotect.java
+++ src/main/java/it/leonardomontemurro/librepdf/core/Unprotect.java
@@ -21,7 +21,6 @@ package it.leonardomontemurro.librepdf.core;
 import org.apache.pdfbox.Loader;
 import org.apache.pdfbox.pdmodel.PDDocument;
 
-
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
