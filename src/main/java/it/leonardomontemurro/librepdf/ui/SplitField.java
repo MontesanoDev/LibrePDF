6962ce5c9ff806ddfe6a77cb2ac3a784ba4f82a5
Leonardo Montemurro <221404985+MontesanoDev@users.noreply.github.com>
2026-04-19 18:28:03 +0200

    feat: added splitfield class

---
+++ src/main/java/it/leonardomontemurro/librepdf/ui/SplitField.java
@@ -0,0 +1,48 @@
+/*
+ * LibrePDF - A lightweight, native tool for manipulating PDF files.
+ * Copyright (C) 2026 Leonardo Montemurro
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package it.leonardomontemurro.librepdf.ui;
+
+import it.leonardomontemurro.librepdf.util.I18N;
+import javafx.geometry.Pos;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TextFormatter;
+
+public class SplitField extends TextField{
+
+    public SplitField() {
+
+        this.setAlignment(Pos.CENTER);
+        this.setPromptText(I18N.get("split.range.info"));
+        bindIntegersToTextField();
+    }
+
+    private void bindIntegersToTextField() {
+        TextFormatter<String> rangeFormatter = new TextFormatter<>(change -> {
+            String newText = change.getControlNewText();
+
+            if (newText.matches("[\\d\\s,-]*")) {
+                return change;
+            }
+            return null;
+        });
+
+        this.setTextFormatter(rangeFormatter);
+    }
+
+}
